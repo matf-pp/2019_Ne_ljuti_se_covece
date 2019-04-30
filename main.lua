236a1e420e1eb6084c94385c11e43f73e2e14677
zizi11 <48031210+zizi11@users.noreply.github.com>
2019-04-30 12:06:43 +0200

    Update main.lua
    Ubacena muzika u pozadini... I jos neke sitne izmene

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+local muzika
+local jacina = 0.3
+
 local pozadina
 
 local kamila
@@ -14,15 +17,15 @@ local pijuni  ----> tabela u kojoj su smestene slike zivotinja
 local tabla = {
     {sirina=100,visina=160}, {sirina=100,visina=250}, {sirina=100,visina=340}, {sirina=100,visina=430}, {sirina=160,visina=470}, {sirina=230,visina=440}, {sirina=300,visina=400}, {sirina=380,visina=400}, {sirina=420,visina=320}, {sirina=400,visina=240}, {sirina=400,visina=160}, {sirina=460,visina=120}, {sirina=520,visina=130}, {sirina=590,visina=120}, {sirina=650,visina=120}, {sirina=650,visina=210}, {sirina=600,visina=260}, {sirina=550,visina=320}, {sirina=580,visina=400}, {sirina=650,visina=440}, {sirina=720,visina=410}, {sirina=790,visina=360}, {sirina=770,visina=280}
 }  ----> po kojoj mrdamo igrace
-
+local igraci = {}
 local igraci2   ----> ako se odabere OPCIJA DVA IGRACA   ----> igraci = igraci2
 local igraci3   ----> ako se odabere OPCIJA TRI IGRACA   ----> igraci = igraci3
 
-local pozicija = 1
+-- local pozicija = 1   -----> STA JE OVO? CINI MI SE DA SE NIGDE NE KORISTI?
 
 --treba da se doda izbor broja igraca, stavljeno podrazumevano dva trenutno(Natalija)
-local brIgraca = 2
-brojac=0
+brojIgraca = 2
+brojac = 0
 local tekst
 local font
 broj=0
@@ -32,111 +35,86 @@ local transform
 
 function love.draw()
 
-    
       love.graphics.draw(pozadina)
       love.graphics.draw(kockica,835,475)
       love.graphics.draw(tekst,15,40)
       love.graphics.draw(tekst2,15,15)
       
-      if brIgraca==2 then
-        love.graphics.draw(igraci2["Prvi"]["Slicica"],tabla[igraci2["Prvi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci2["Prvi"]["PoljeNaKomJe"]]["visina"])
-        love.graphics.draw(igraci2["Drugi"]["Slicica"],tabla[igraci2["Drugi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci2["Drugi"]["PoljeNaKomJe"]]["visina"])
+      
+      
+      if brojIgraca==2 then
+        love.graphics.draw(igraci["Prvi"]["Slicica"],tabla[igraci["Prvi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci["Prvi"]["PoljeNaKomJe"]]["visina"])
+        love.graphics.draw(igraci["Drugi"]["Slicica"],tabla[igraci["Drugi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci["Drugi"]["PoljeNaKomJe"]]["visina"])
       else
-        love.graphics.draw(igraci3["Prvi"]["Slicica"],tabla[igraci3["Prvi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci3["Prvi"]["PoljeNaKomJe"]]["visina"])
-        love.graphics.draw(igraci3["Drugi"]["Slicica"],tabla[igraci3["Drugi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci3["Drugi"]["PoljeNaKomJe"]]["visina"])
-        love.graphics.draw(igraci3["Treci"]["Slicica"],tabla[igraci3["Treci"]["PoljeNaKomJe"]]["sirina"],tabla[igraci3["Treci"]["PoljeNaKomJe"]]["visina"])
+        love.graphics.draw(igraci["Prvi"]["Slicica"],tabla[igraci["Prvi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci["Prvi"]["PoljeNaKomJe"]]["visina"])
+        love.graphics.draw(igraci["Drugi"]["Slicica"],tabla[igraci["Drugi"]["PoljeNaKomJe"]]["sirina"],tabla[igraci["Drugi"]["PoljeNaKomJe"]]["visina"])
+        love.graphics.draw(igraci["Treci"]["Slicica"],tabla[igraci["Treci"]["PoljeNaKomJe"]]["sirina"],tabla[igraci["Treci"]["PoljeNaKomJe"]]["visina"])
       end
       
+----> izmenila sam da se uvek poziva sa igraci(dole ima provera za koliko igraca i postavi odgovarajucu tabelu) umesto igraci2 i igraci3 (bilo mi je lakse da se snadjem...)
 
-  if brIgraca==2 then
-    if math.fmod(brojac,brIgraca)==0 then
-      tekst:set(igraci2["Prvi"]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci2["Prvi"]
-      if igraci2["Prvi"]["PoljeNaKomJe"]+broj<=23 then
-        igraci2["Prvi"]["PoljeNaKomJe"]=igraci2["Prvi"]["PoljeNaKomJe"]+broj
+  if brojIgraca == 2 then
+    if math.fmod(brojac,brojIgraca)==0 then
+      tekst:set(igraci["Prvi"]["Ime"].." je na redu. Baci kockicu!",25,25)
+      igrac=igraci["Prvi"]
+      if igraci["Prvi"]["PoljeNaKomJe"] + broj <= 23 then
+          igraci["Prvi"]["PoljeNaKomJe"]=igraci["Prvi"]["PoljeNaKomJe"]+broj
       end
     else
-      tekst:set(igraci2["Drugi"]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci2["Drugi"]
-      if igraci2["Drugi"]["PoljeNaKomJe"]+broj<=23 then
-        igraci2["Drugi"]["PoljeNaKomJe"]=igraci2["Drugi"]["PoljeNaKomJe"]+broj
+      tekst:set(igraci["Drugi"]["Ime"].." je na redu. Baci kockicu!",25,25)
+      igrac=igraci["Drugi"]
+      if igraci["Drugi"]["PoljeNaKomJe"]+broj<=23 then
+        igraci["Drugi"]["PoljeNaKomJe"]=igraci["Drugi"]["PoljeNaKomJe"]+broj
       end
     end
   else
-    if math.fmod(brojac,brIgraca)==0 then
-      tekst:set(igraci3["Prvi"]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci3["Prvi"]
-    elseif math.fmod(brojac,brIgraca)==1 then
-      tekst:set(igraci3["Drugi"]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci3["Drugi"]
+    if math.fmod(brojac,brojIgraca)==0 then
+      tekst:set(igraci["Prvi"]["Ime"].." je na redu. Baci kockicu!",25,25)
+      igrac=igraci["Prvi"]
+      if igraci["Prvi"]["PoljeNaKomJe"] + broj <= 23 then
+          igraci["Prvi"]["PoljeNaKomJe"]=igraci["Prvi"]["PoljeNaKomJe"]+broj   --->zaboravila si ovde provere i kretanje
+      end
+    else if math.fmod(brojac,brojIgraca)==1 then
+      tekst:set(igraci["Drugi"]["Ime"].." je na redu. Baci kockicu!",25,25)
+      igrac=igraci["Drugi"]
+      if igraci["Drugi"]["PoljeNaKomJe"]+broj<=23 then
+        igraci["Drugi"]["PoljeNaKomJe"]=igraci["Drugi"]["PoljeNaKomJe"]+broj  --->zaboravila si ovde provere i kretanje
+      end
     else
-      tekst:set(igraci3["Treci"]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci3["Treci"]
+      tekst:set(igraci["Treci"]["Ime"].." je na redu. Baci kockicu!",25,25)                                 
+      igrac=igraci["Treci"]
+      if igraci["Treci"]["PoljeNaKomJe"] + broj <= 23 then
+          igraci["Treci"]["PoljeNaKomJe"]=igraci["Treci"]["PoljeNaKomJe"]+broj    --->zaboravila si ovde provere i kretanje 
+      end
+         end
     end
-  end
-  
-  function love.mousepressed(x,y,button,isTouched)
+ end
+ 
+end
+function love.mousepressed(x,y,button,isTouched)
     if x>835 and x<950 and y>475 and y<600 then
       izracunaj()
-    else
     end
-  end
-  
-  
-
-  
-      
---       love.graphics.draw(kamila,100,100)
---       love.graphics.draw(koala,650,400)   ----> zivotinje na random mestima       
---       love.graphics.draw(lav,400,400)
-
---[[
-    love.graphics.draw(lisica,100,160)
-    love.graphics.draw(lisica,100,250)
-    love.graphics.draw(lisica,100,340)
-    love.graphics.draw(lisica,100,430)   ]]
---     love.graphics.draw(lisica,160,470)
---[[   
-    love.graphics.draw(lisica,230,440)
-    love.graphics.draw(lisica,300,400)
-    love.graphics.draw(lisica,380,400)
-    love.graphics.draw(lisica,420,320)
-    love.graphics.draw(lisica,400,240)
-    love.graphics.draw(lisica,400,160)
-    love.graphics.draw(lisica,460,120)
-    love.graphics.draw(lisica,520,130)
-    love.graphics.draw(lisica,590,120)
-    love.graphics.draw(lisica,650,120)
-    love.graphics.draw(lisica,650,210)      ----> put sam po lisici pravila
-    love.graphics.draw(lisica,600,260)
-    love.graphics.draw(lisica,550,320)
-    love.graphics.draw(lisica,580,400)
-    love.graphics.draw(lisica,650,440)
-    love.graphics.draw(lisica,720,410)
-    love.graphics.draw(lisica,790,360)
-    love.graphics.draw(lisica,770,280) ]]
-
---    for pozicija=1,23 do      --- zakomentarisi ovaj red (**)
---     love.graphics.draw(zirafa,tabla[pozicija].sirina,tabla[pozicija].visina)     --> ako zelite da vidite zirafu na putu
---    end                       --- zakomentarisi ovaj red (**)
-
---      love.graphics.draw(zirafa,100,300)
---      love.graphics.draw(rakun,500,200)    ---> zivotinje iscrtane na random mestima
---      love.graphics.draw(zec,650,100)
---      love.graphics.draw(panda,750,100)
-      
-      
 end
+  
+        
 
 function izracunaj()
     math.randomseed(os.time())
     broj=math.random(6)
     tekst2:set(igrac["Ime"].." dobili ste broj "..tostring(broj).."!")
+    
     brojac=brojac+1
     
   end
 
 function love.load() 
+    
+       love.audio.setVolume(jacina)                      
+       muzika = love.audio.newSource("pesma.mp3","stream") 
+       muzika:setLooping(true)
+       muzika:play()                                                -----> podesavanje muzike
+    
        pozadina = love.graphics.newImage("PozadinaSaPoljima.jpeg")   ---> ucitala pozadinu
 
        kamila = love.graphics.newImage("kamila.png")
@@ -154,12 +132,7 @@ function love.load()
        tekst2=love.graphics.newText(font,"")
 
        
-pijuni= {kamila,koala,lav,lisica,panda,rakun,zec,zirafa}
-
-
-
-    
-
+pijuni= {kamila,koala,lav,lisica,panda,rakun,zec,zirafa}  
 
 igraci2 = {
    Prvi = { Ime = "Prvi Igrac", PoljeNaKomJe = 1, Slicica = pijuni[1]},
@@ -171,6 +144,14 @@ igraci3 = {
    Drugi = { Ime = "Drugi Igrac", PoljeNaKomJe = 1, Slicica = pijuni[2]},
    Treci = { Ime = "Treci Igrac", PoljeNaKomJe = 1, Slicica = pijuni[3]}    
 }
+
+
+if(brojIgraca == 2) then
+    igraci = igraci2
+else if(brojIgraca == 3) then
+        igraci = igraci3
+     end
+end
 
 pitanja1={
   {"Grcka boginja ljubavi zove se: ","a) Atina","b) Afrodita", "c) Venera", "d) Hera", 2},
@@ -188,11 +169,17 @@ pitanja2={
   {"Broj 100 pisan rimskim brojevima je:","a) L","b) M","c) D","d) C",4},
   {"Formula za racunanje povrsine kvadrata glasi:","a) a+a","b) a-b","c) a*b","d) a*a",4}}
 
+end
 
 
--- pristup igracu: npr.  igraci.Prvi.Ime   ILI   igraci["Prvi"]["Ime"]
---   izmena argumenta moze: npr. igraci.Prvi.PoljeNaKomJe = 5
 
+function love.keypressed(key)
+    
+   if (key == "m") then jacina = 0.0 end
+   if (key == "u") then jacina = 0.3 end               ---> mute i unmute za pozadinsku muziku ---> klikom na m tj. na u
+   
+   love.audio.setVolume(jacina)
+   
 end
 
 
@@ -202,7 +189,4 @@ end
 
 function love.update(dt)
 
---     if pozicija < 23 then
---         pozicija = pozicija + 1    ---> brzinom svetlosti iscrta zirafu xD ( ali zakomentarisite for i end gore gde je (**) )
---     end
 end
