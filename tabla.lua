de222f4514a2dfa9e3be78ff468d117cbeb9bb5e
nevera97 <47864981+nevera97@users.noreply.github.com>
2019-05-04 15:34:42 +0200

    Update tabla.lua

--- tabla.lua
+++ tabla.lua
@@ -5,7 +5,7 @@ muzika:setLooping(true)
 muzika:play()                                                -----> podesavanje muzike
     
 pozadina = love.graphics.newImage("PozadinaSaPoljima.jpeg")   ---> ucitala pozadinu
-
+pozadinap = love.graphics.newImage("pocetna.jpg") 
 kamila = love.graphics.newImage("kamila.png")
 koala = love.graphics.newImage("koala.png") 
 lav = love.graphics.newImage("lav.png")                 -----> ucitala zivotinjice (slike)
@@ -31,90 +31,9 @@ tabla = {
 } 
 
 brojac=0
+prom = "tabla"
 
-function love.draw()
-
-      love.graphics.draw(pozadina)
-      love.graphics.draw(kockica,835,475)
-      love.graphics.draw(tekst,15,40)
-      love.graphics.draw(tekst2,15,15)
-      love.graphics.draw(tekst3,280,300)
-      love.graphics.draw(tekst4,300,15)
-      
-      love.graphics.draw(igraci[1]["Slicica"],tabla[igraci[1]["PoljeNaKomJe"]]["sirina"],tabla[igraci[1]["PoljeNaKomJe"]]["visina"])
-      love.graphics.draw(igraci[2]["Slicica"],tabla[igraci[2]["PoljeNaKomJe"]]["sirina"],tabla[igraci[2]["PoljeNaKomJe"]]["visina"])
-      if brojIgraca==3 then
-          love.graphics.draw(igraci[3]["Slicica"],tabla[igraci[3]["PoljeNaKomJe"]]["sirina"],tabla[igraci[3]["PoljeNaKomJe"]]["visina"])
-      end
-      
-----> izmenila sam da se uvek poziva sa igraci(dole ima provera za koliko igraca i postavi odgovarajucu tabelu) umesto igraci2 i igraci3 (bilo mi je lakse da se snadjem...)
-if (brojac % brojIgraca) == 0 then
-         tekst:set(igraci[1]["Ime"].." je na redu. Baci kockicu!",25,25)
-         igrac=igraci[1]
-         redniBroj=1
-    else if (brojac % brojIgraca) == 1 then
-      tekst:set(igraci[2]["Ime"].." je na redu. Baci kockicu!",25,25)
-      igrac=igraci[2]
-      redniBroj=2
-         else 
-             tekst:set(igraci[3]["Ime"].." je na redu. Baci kockicu!",25,25)
-             igrac=igraci[3]
-             redniBroj=3
-         end 
-    end
- 
-end
-
-function love.mousepressed(x,y,button,isTouched)
-    if x>835 and x<950 and y>475 and y<600 then
-      izracunaj()
-    end
-end
-        
-
-function izracunaj()
-    broj=math.random(6)
-    tekst2:set(igrac["Ime"].." dobili ste broj "..tostring(broj).."!")
-    brojac=brojac+1
-   if igrac["PoljeNaKomJe"] + broj > 23 then
-        neMozeDalje(igrac)
-   end
-   if igrac["PoljeNaKomJe"] + broj == 23 then
-        korak=igrac["PoljeNaKomJe"]+broj
-        igrac["PoljeNaKomJe"]=korak
-        cestitamo(igrac)
-   end
-   if igrac["PoljeNaKomJe"] + broj < 23 then
-          korak=igrac["PoljeNaKomJe"]+broj
-          igrac["PoljeNaKomJe"]=korak
-          for k,v in pairs(igraci) do
-              if k ~= redniBroj then
-                 if igraci[k]["PoljeNaKomJe"] == korak then
-                    igraci[k]["PoljeNaKomJe"]=1
-                    pojedenSi(igrac,igraci[k])
-                 end
-              end      
-          end       
-   end
-   
-end
-
-function pojedenSi(igrac,pojedeni)
-     tekst4:set(igrac["Ime"]..", pojeo si " .. pojedeni["Ime"] .. "!") ------> moze nastati problem ako igraci zadaju predugacko ime, zato OGRANICI IME IGRACA! [mozda ce se preklopiti delom tekstovi u suprotnom]
-end
-
-function neMozeDalje(igrac)
-     tekst4:set(igrac["Ime"]..", zao mi je, ne mozes dalje...")------> moze nastati problem ako igraci zadaju predugacko ime, zato OGRANICI IME IGRACA! [mozda ce se preklopiti delom tekstovi u suprotnom]
-end
-
-function cestitamo(igrac)
-  pobednik=igrac
-  require("cestitamo")
-  -----> ovde jos nisam blokirala da nastave da udaraju kockicu i da se krecu ostali dok ne stignu do cilja.... [ALI TU IDE NOVI PROZOR? i tamo cete ispisati cestitamo,igraj ponovo/izadji?]
-end
-
-
-pitanja={
+pitanjal={
   {},
   {"Grcka boginja ljubavi zove se: ","a) Atina","b) Afrodita", "c) Venera", "d) Hera", 2},
   {"Broj 100 pisan rimskim brojevima je:","a) L","b) M","c) D","d) C",4},
@@ -139,6 +58,143 @@ pitanja={
   {"Druge zove sebe ne cuje. Sta je to?", "a) zvono", "b) glas","c) zvuk", "d) ime",1},
   {}}
 
+ local odgovorIgraca
+
+function love.draw()
+
+    if prom == "tabla" then 
+      love.graphics.setColor(1, 1, 1, 1)
+      love.graphics.draw(pozadina)
+      love.graphics.draw(kockica,835,475)
+      love.graphics.draw(tekst,15,40)
+      love.graphics.draw(tekst2,15,15)
+      love.graphics.draw(tekst3,280,300)
+      love.graphics.draw(tekst4,300,15)
+
+      
+      love.graphics.draw(igraci[1]["Slicica"],tabla[igraci[1]["PoljeNaKomJe"]]["sirina"],tabla[igraci[1]["PoljeNaKomJe"]]["visina"])
+      love.graphics.draw(igraci[2]["Slicica"],tabla[igraci[2]["PoljeNaKomJe"]]["sirina"],tabla[igraci[2]["PoljeNaKomJe"]]["visina"])
+      if brojIgraca==3 then
+          love.graphics.draw(igraci[3]["Slicica"],tabla[igraci[3]["PoljeNaKomJe"]]["sirina"],tabla[igraci[3]["PoljeNaKomJe"]]["visina"])
+      end
+      
+----> izmenila sam da se uvek poziva sa igraci(dole ima provera za koliko igraca i postavi odgovarajucu tabelu) umesto igraci2 i igraci3 (bilo mi je lakse da se snadjem...)
+if (brojac % brojIgraca) == 0 then
+         tekst:set(igraci[1]["Ime"].." je na redu. Baci kockicu!",25,25)
+         igrac=igraci[1]
+         redniBroj=1
+    else if (brojac % brojIgraca) == 1 then
+      tekst:set(igraci[2]["Ime"].." je na redu. Baci kockicu!",25,25)
+      igrac=igraci[2]
+      redniBroj=2
+         else 
+             tekst:set(igraci[3]["Ime"].." je na redu. Baci kockicu!",25,25)
+             igrac=igraci[3]
+             redniBroj=3
+         end 
+    end
+  end
+
+  if prom == "pitanje" then 
+
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.draw(pozadinap)
+
+  love.graphics.setColor(255/255,255/255,153/255,1)
+  love.graphics.rectangle('fill', 200, 100, 300, 150,35,35)
+  love.graphics.rectangle('fill', 100, 300, 200, 60, 35, 35)
+  love.graphics.rectangle('fill', 400, 300,200, 60, 35, 35)
+  love.graphics.rectangle('fill', 100, 450, 200, 60, 35, 35)
+  love.graphics.rectangle('fill', 400, 450, 200, 60, 35, 35)
+
+  love.graphics.setColor(0, 0, 255, 100)
+  love.graphics.printf(pitanjal[igrac["PoljeNaKomJe"]][1],250,120,200)
+  love.graphics.printf(pitanjal[igrac["PoljeNaKomJe"]][2],150,320,150)
+  love.graphics.printf(pitanjal[igrac["PoljeNaKomJe"]][3],450,320,150)
+  love.graphics.printf(pitanjal[igrac["PoljeNaKomJe"]][4],150,470,150)
+  love.graphics.printf(pitanjal[igrac["PoljeNaKomJe"]][5],450,470,150)
+ 
+end
+end
+
+function love.mousepressed(x,y,button,isTouched)
+    if prom == "tabla" and x>835 and x<950 and y>475 and y<600 then
+      izracunaj()
+    end
+
+    if prom=="pitanje" and x>100 and x<300 and y>300 and y<360 then 
+    odgovorIgraca = 1
+    prom = "tabla"
+  end
+
+  if prom =="pitanje" and x>400 and x<600 and y>300 and y<360 then 
+    odgovorIgraca = 2
+    prom = "tabla"
+  end
+
+  if prom == "pitanje" and x>100 and x<300 and y>450 and y<510 then 
+    odgovorIgraca = 3
+    prom = "tabla"
+  end
+
+  if prom == "pitanje" and x>400 and x<600 and y>450 and y<510 then 
+    odgovorIgraca = 4
+    prom = "tabla"
+  end
+end
+        
+
+function izracunaj()
+    broj=math.random(6)
+    tekst2:set(igrac["Ime"].." dobili ste broj "..tostring(broj).."!")
+    brojac=brojac+1
+   if igrac["PoljeNaKomJe"] + broj > 23 then
+        neMozeDalje(igrac)
+   end
+   if igrac["PoljeNaKomJe"] + broj == 23 then
+        korak=igrac["PoljeNaKomJe"]+broj
+        igrac["PoljeNaKomJe"]=korak
+        cestitamo(igrac)
+   end
+   if igrac["PoljeNaKomJe"] + broj < 23 then
+          prom = "pitanje"
+          if odgovorIgraca == pitanjal[igrac["PoljeNaKomJe"]][6] then 
+          korak=igrac["PoljeNaKomJe"]+broj
+          else
+            korak=igrac["PoljeNaKomJe"]+0
+          end
+
+          igrac["PoljeNaKomJe"]=korak
+          for k,v in pairs(igraci) do
+              if k ~= redniBroj then
+                 if igraci[k]["PoljeNaKomJe"] == korak then
+                    igraci[k]["PoljeNaKomJe"]=1
+                    pojedenSi(igrac,igraci[k])
+                 end
+              end      
+          end
+                 
+   end
+   
+end
+
+function pojedenSi(igrac,pojedeni)
+     tekst4:set(igrac["Ime"]..", pojeo si " .. pojedeni["Ime"] .. "!") ------> moze nastati problem ako igraci zadaju predugacko ime, zato OGRANICI IME IGRACA! [mozda ce se preklopiti delom tekstovi u suprotnom]
+end
+
+function neMozeDalje(igrac)
+     tekst4:set(igrac["Ime"]..", zao mi je, ne mozes dalje...")------> moze nastati problem ako igraci zadaju predugacko ime, zato OGRANICI IME IGRACA! [mozda ce se preklopiti delom tekstovi u suprotnom]
+end
+
+function cestitamo(igrac)
+  pobednik=igrac
+  require("cestitamo")
+  -----> ovde jos nisam blokirala da nastave da udaraju kockicu i da se krecu ostali dok ne stignu do cilja.... [ALI TU IDE NOVI PROZOR? i tamo cete ispisati cestitamo,igraj ponovo/izadji?]
+end
+
+
+
+
 
 
 
@@ -159,3 +215,4 @@ end
 function love.update(dt)
 
 end
+
